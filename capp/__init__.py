7857c9843e4a31be918b48e210f5c14aaa33dab7
cecham1 <159899570+cecham1@users.noreply.github.com>
2024-02-26 13:45:51 +0100

    Update __init__.py

--- capp/__init__.py
+++ capp/__init__.py
@@ -5,6 +5,8 @@ application = Flask(__name__)
 
 application.config['SECRET_KEY'] = os.environ['SECRET_KEY']  
 
+application.config['SECRET_KEY'] = '3daed8d179e2a254c691f3d6b2104484ade7aa315221bfbd'
+
 from capp.home.routes import home
 from capp.methodology.routes import methodology
 from capp.carbon_app.routes import carbon_app
