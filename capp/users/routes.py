41f0c49a2434deb4665b7d37c244d7859b1f22ea
cecham1 <159899570+cecham1@users.noreply.github.com>
2024-04-08 20:17:40 +0200

    Update routes.py

--- capp/users/routes.py
+++ capp/users/routes.py
@@ -1,33 +1,33 @@
-from flask import render_template, Blueprint, redirect, flash, url_for
-from capp.users.forms import RegistrationForm, LoginForm
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from capp.models import User
-from capp import db, bcrypt
-
-users=Blueprint('users',__name__)
-
-@users.route('/register', methods=['GET','POST'])
-def register():
-  form = RegistrationForm()
-  if form.validate_on_submit():
-      user_hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-      user = User(username=form.username.data, email=form.email.data, password=user_hashed_password)
-      db.session.add(user)
-      db.session.commit()
-      flash('Your account has been created! Now, you are able to login!', 'success')
-      return redirect(url_for('home.home_home'))
-  return render_template('users/register.html', title='register', form=form)
-
-@users.route('/login', methods=['GET','POST'])
-def login():
-  form = LoginForm()
-  if form.validate_on_submit():
-    if form.email.data == 'fjell@demo.com' and form.email.data == 'regn':
-        flash('You have logged in! Now, you can start to use Forward!', 'success')
-        return redirect(url_for('forward_home.home'))
-    else:
-        flash('Login Unsuccessful. Please check email and password!', 'danger')  
-  return render_template('users/login.html', title='login', form=form)
 
 
+class RegistrationForm(FlaskForm):
+  username = StringField('Username', 
+                         validators=[DataRequired(), Length(min=2, max=30)])
+  email = StringField('Email',
+                      validators=[DataRequired(), Email()])
+  password = PasswordField('Password', validators=[DataRequired()])
+  confirm_password = PasswordField('Confirm Password',
+                      validators=[DataRequired(), EqualTo('password')])                                
+  submit = SubmitField('Sign Up')
 
+  def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken. Please choose a different one.')
+
+  def validate_email(self, email):
+      user = User.query.filter_by(email=email.data).first()
+      if user:
+          raise ValidationError('That email is taken. Please choose a different one.')
+
+class LoginForm(FlaskForm):
+  email = StringField('Email',
+                      validators=[DataRequired(), Email()])
+  password = PasswordField('Password', validators=[DataRequired()])
+  remember = BooleanField('Remember Me')
+  submit = SubmitField('Login') 
 
