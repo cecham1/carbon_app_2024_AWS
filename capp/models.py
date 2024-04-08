eab9eb080ca46efc8f8df880c4caa2629c6ec5f7
cecham1 <159899570+cecham1@users.noreply.github.com>
2024-04-08 20:20:28 +0200

    Update models.py

--- capp/models.py
+++ capp/models.py
@@ -1,8 +1,13 @@
-from capp import db
+from capp import db, login_manager
 from datetime import datetime
+from flask_login import UserMixin
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 # Database User
-class User(db.Model):
+class User(db.Model, UserMixin):
     __tablename__ = "user_table"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(30), unique=True, nullable=False)
