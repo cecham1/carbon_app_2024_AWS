23a48cbfef081968ab0c67b04fb94872cb657331
cecham1 <159899570+cecham1@users.noreply.github.com>
2024-04-09 11:00:29 +0200

    Add files via upload

--- capp/users/routes.py
+++ capp/users/routes.py
@@ -1,33 +1,44 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flask import render_template, Blueprint, redirect, flash, url_for, request
+from capp.users.forms import RegistrationForm, LoginForm
 from capp.models import User
+from capp import db, bcrypt
+from flask_login import login_user, current_user, logout_user
 
+users=Blueprint('users',__name__)
 
-class RegistrationForm(FlaskForm):
-  username = StringField('Username', 
-                         validators=[DataRequired(), Length(min=2, max=30)])
-  email = StringField('Email',
-                      validators=[DataRequired(), Email()])
-  password = PasswordField('Password', validators=[DataRequired()])
-  confirm_password = PasswordField('Confirm Password',
-                      validators=[DataRequired(), EqualTo('password')])                                
-  submit = SubmitField('Sign Up')
+@users.route('/register', methods=['GET','POST'])
+def register():
+  form = RegistrationForm()
+  if current_user.is_authenticated:
+        return redirect(url_for('home.home_home'))
+  if form.validate_on_submit():
+      user_hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+      user = User(username=form.username.data, email=form.email.data, password=user_hashed_password)
+      db.session.add(user)
+      db.session.commit()
+      flash('Your account has been created! Now, you are able to login!', 'success')
+      return redirect(url_for('users.login'))
+  return render_template('users/register.html', title='register', form=form)
 
-  def validate_username(self, username):
-        user = User.query.filter_by(username=username.data).first()
-        if user:
-            raise ValidationError('That username is taken. Please choose a different one.')
+@users.route('/login', methods=['GET','POST'])
+def login():
+  form = LoginForm()
+  if current_user.is_authenticated:
+        return redirect(url_for('home.home_home'))
+  if form.validate_on_submit():
+    user = User.query.filter_by(email=form.email.data).first()
+    if user and bcrypt.check_password_hash(user.password, form.password.data):
+        login_user(user, remember=form.remember.data)
+        next_page = request.args.get('next')
+        flash('You have logged in! Now, you can start to use our Carbon App!', 'success')
+        return redirect(next_page) if next_page else redirect(url_for('home.home_home'))
+    else:
+        flash('Login Unsuccessful. Please check email and password!', 'danger') 
+  return render_template('users/login.html', title='login', form=form)
 
-  def validate_email(self, email):
-      user = User.query.filter_by(email=email.data).first()
-      if user:
-          raise ValidationError('That email is taken. Please choose a different one.')
+@users.route('/logout')
+def logout():    
+    logout_user()
+    return redirect(url_for('home.home_home'))
 
-class LoginForm(FlaskForm):
-  email = StringField('Email',
-                      validators=[DataRequired(), Email()])
-  password = PasswordField('Password', validators=[DataRequired()])
-  remember = BooleanField('Remember Me')
-  submit = SubmitField('Login') 
 
